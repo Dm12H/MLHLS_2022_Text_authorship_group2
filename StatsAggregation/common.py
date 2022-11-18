095ef8468f6d7437a0786e01dff4e8375247f0e2
Vigolod <vladimir.golod22@gmail.com>
2022-11-18 15:54:46 +0200

    New features and rework on old ones

--- StatsAggregation/common.py
+++ StatsAggregation/common.py
@@ -13,6 +13,7 @@ from ebooklib import epub
 
 from visualizers import draw_distribution, draw_ridge3d
 
+delete_inner = str.maketrans('', '', '\u0301\u0300\u0060\u02cb\u0027\u2019\u002d\u2010')
 
 def get_paragraphs(chapter):
     soup = BeautifulSoup(chapter.get_body_content(), 'html.parser')
@@ -62,28 +63,54 @@ def hash_results(func, data_dir, writer, *args, out_path=None, **kwargs):
         writers_hashfile.write(f)
 
 
-def get_feature_sample(func):
-    def wrapper(writer, writers_dir, step=100):
-        chapters = get_books_as_text_iterator(writer, writers_dir)
-        chapters_to_sentences = map(lambda chp: nltk.sent_tokenize(' '.join(chp), language='russian'), chapters)
-        sentences = list(it.chain.from_iterable(chapters_to_sentences))
-
-        sample = []
-        for i in range(0, len(sentences), step):
-            sample.append(func(sentences[i: i + step]))
-        return sample
-    return wrapper
-
-
 @lru_cache(maxsize=1)
-def sentences_per_chpt(writer, writers_dir, sample_step=100, **__):
+def sentence_batches(writer, writers_dir, sentences_in_batch=100, **_):
     chapters = get_books_as_text_iterator(writer, writers_dir)
     chapters_to_sentences = map(lambda chp: nltk.sent_tokenize(' '.join(chp), language='russian'), chapters)
     sentences = list(it.chain.from_iterable(chapters_to_sentences))
     samples = []
-    for i in range(0, len(sentences), sample_step):
-        samples.append(sentences[i:i + sample_step])
+    for i in range(0, len(sentences), sentences_in_batch):
+        samples.append(sentences[i:i + sentences_in_batch])
     return samples
+
+
+@lru_cache(maxsize=1)
+def paragraphs_limmited_by_symbols(writer, writers_dir, symbol_lim=50000, **_):
+    paragraphs = list(it.chain.from_iterable(get_books_as_text_iterator(writer, writers_dir)))
+    samples = []
+    i = 0
+    while i < len(paragraphs):
+        new_sample = []
+        symbol_cnt = 0
+        while symbol_cnt < symbol_lim and i < len(paragraphs):
+            new_sample.append(paragraphs[i])
+            symbol_cnt += len(paragraphs[i])
+            i += 1
+        samples.append(new_sample)
+    return samples
+
+
+@lru_cache(maxsize=1)
+def token_batches(writer, writers_dir, tokens_in_batch=2000, **_):
+    chapters = get_books_as_text_iterator(writer, writers_dir)
+    chapters_to_tokens = map(lambda chp: nltk.word_tokenize(' '.join(chp), language='russian'), chapters)
+    tokens = list(it.chain.from_iterable(chapters_to_tokens))
+    sample = []
+    for i in range(0, len(tokens), tokens_in_batch):
+        sample.append(tokens[i:i + tokens_in_batch])
+    return sample
+
+
+@lru_cache(maxsize=1)
+def word_batches(writer, writers_dir, words_in_batch=2000, **_):
+    chapters = get_books_as_text_iterator(writer, writers_dir)
+    chapters_to_tokens = map(lambda chp: nltk.word_tokenize(' '.join(chp), language='russian'), chapters)
+    tokens = it.chain.from_iterable(chapters_to_tokens)
+    words = words_from_tokens(tokens)
+    sample = []
+    for i in range(0, len(words), words_in_batch):
+        sample.append(words[i: i + words_in_batch])
+    return sample
 
 
 class Feature:
@@ -126,7 +153,7 @@ class Feature:
 
 class ScalarFeature(Feature):
 
-    data_source = sentences_per_chpt
+    data_source = sentence_batches
     visualizer_single = draw_distribution
     visualizer_all = draw_ridge3d
 
@@ -143,3 +170,6 @@ class FeatureList:
     def register_feature(cls, feature):
         cls.features.append(feature)
         return feature
+
+def words_from_tokens(tokens):
+    return [token for token in tokens if token.translate(delete_inner).isalpha()]
