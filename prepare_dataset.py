c430fc55474f7fcc6a8f80b6f57fd17c4b5fbf47
Максим Демидов <myudemidov@edu.hse.ru>
2023-06-27 18:03:16 +0300

    silence prints when app is running

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import os
 import argparse
-import pickle
+import sys
 from typing import Union
 
-import pandas as pd
 
 from text_authorship.ta_data_extraction.data_extraction import extract_df
 from text_authorship.ta_model.data_preparation import TATransformer, load_df
@@ -14,23 +13,27 @@ def prepare_dataset(data: str,
                     output_dir: str,
                     parser: Union[str, None] = None,
                     symbol_lim: int = 3000):
+    if __name__ == "__main__":
+        f_out = sys.stdout
+    else:
+        f_out = os.devnull
     if not os.path.exists(data):
         raise ValueError("provided data_dir does not exist")
     if os.path.isdir(data):
-        print("READING DATA FROM WRITERS DIRECTORY")
+        print("READING DATA FROM WRITERS DIRECTORY", file=f_out)
         df = extract_df(data, symbol_lim=symbol_lim)
     else:
-        print("READING DATA FROM EXISTING DF")
+        print("READING DATA FROM EXISTING DF", file=f_out)
         columns = ["author", "book", "text"]
         df = load_df(data, columns=columns).iloc[:50]
     transformer = TATransformer(parser=parser)
-    print("FINISHED READING, PREPARING FEATURES")
+    print("FINISHED READING, PREPARING FEATURES", file=f_out)
     df = transformer.transform(df)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     output_path = os.path.join(output_dir, "prepared_df.csv")
     df.to_csv(output_path)
-    print(f"DATASET SAVED AT: {output_path}")
+    print(f"DATASET SAVED AT: {output_path}", file=f_out)
 
 
 if __name__ == "__main__":
